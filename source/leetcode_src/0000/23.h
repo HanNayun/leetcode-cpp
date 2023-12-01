cc032cb54818ab98859aa5dd0aef473063af2183
nayun <han.nayun@icloud.com>
2023-12-01 11:58:01 +0800

    feat: 12/1/2023

---
+++ source/leetcode_src/0000/23.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "include/list_node.h"
+
+#include<queue>
+#include<algorithm>
+
+namespace leetcode_23{
+class Solution
+{
+public:
+    ListNode* mergeKLists(std::vector<ListNode*>& lists)
+    {
+        return MergeLists(lists, 0, lists.size() - 1);
+    }
+
+    ListNode* MergeLists(std::vector<ListNode*>& lists, int l, int r)
+    {
+        if (l == r)
+            return lists[l];
+
+        if (l > r)
+            return nullptr;
+
+        int mid = (l + r) / 2;
+        return mergeTwoLists(MergeLists(lists, l, mid), MergeLists(lists, mid + 1, r));
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
+        if ((!list1) || (!list2))
+            return list1 == nullptr ? list2 : list1;
+
+        auto dummy = new ListNode;
+        const auto res = dummy;
+        ListNode* next = nullptr;
+        while (list1 && list2)
+        {
+            if (list1->val < list2->val)
+            {
+                next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                next = list2;
+                list2 = list2->next;
+            }
+
+            dummy->next = next;
+            dummy = dummy->next;
+            dummy->next = nullptr;
+        }
+
+        dummy->next = list1 == nullptr ? list2 : list1;
+        auto ans = res->next;
+        res->next = nullptr;
+        delete res;
+
+        return ans;
+    }
+};
+}
