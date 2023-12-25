bfd162c703bc9dac8e56a2ba0907bcc876933f8f
nayun <han.nayun@icloud.com>
2023-12-25 15:32:28 +0800

    update: Update 307

--- source/leetcode_src/0300/307.h
+++ source/leetcode_src/0300/307.h
@@ -6,29 +6,32 @@ namespace leetcode_307 {
 class NumArray
 {
 public:
-    NumArray(std::vector<int>& nums)
-            : nums_(nums),
-              tree_(std::vector<int>(nums.size() + 1))
+    explicit NumArray(std::vector<int>& nums)
+            : _nums(nums),
+              _bit(std::vector<int>(nums.size() + 1))
     {
-        for (int i = 0; i < nums.size(); ++i)
-            Add(i + 1, nums[i]);
+        for (int i = 1; i < _bit.size(); ++i)
+        {
+            _bit[i] += _nums[i - 1];
+            auto j = i + LowBit(i);
+            if (j < _bit.size())
+                _bit[j] += _bit[i];
+        }
     }
 
     void update(int index, int val)
     {
-        Add(index + 1, val - nums_[index]);
-        nums_[index] = val;
+        Add(index + 1, val - _nums[index]);
+        _nums[index] = val;
     }
 
+    [[nodiscard]]
     int sumRange(int left, int right) const
     {
         return Query(right + 1) - Query(left);
     }
 
 private:
-    std::vector<int>& nums_;
-    std::vector<int> tree_;
-
     static int LowBit(int x)
     {
         return x & (-x);
@@ -36,9 +39,9 @@ private:
 
     void Add(int idx, int val)
     {
-        while (idx < tree_.size())
+        while (idx < _bit.size())
         {
-            tree_[idx] += val;
+            _bit[idx] += val;
             idx += LowBit(idx);
         }
     }
@@ -49,10 +52,13 @@ private:
         int sum{0};
         while (idx > 0)
         {
-            sum += tree_[idx];
+            sum += _bit[idx];
             idx -= LowBit(idx);
         }
         return sum;
     }
+
+    std::vector<int> _bit;
+    std::vector<int> _nums;
 };
 }
